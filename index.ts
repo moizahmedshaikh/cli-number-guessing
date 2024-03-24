43ce66ef4570d560b95338eaa16583dcb92249d9
moizahmedshaikh <160318328+moizahmedshaikh@users.noreply.github.com>
2024-03-24 09:40:27 -0700

    created a number guessing game.

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log("welcome to Guessing Number Game");
+
+let gameNumber = Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer.prompt([
+    {
+       name : "userGuessedNumber",
+       type : "number",
+       message : "Please guess a number between 1 To 6 :",
+    }
+])
+if(gameNumber === answer.userGuessedNumber){
+    console.log(chalk.yellowBright("Congratulatios! you guessed right number"));
+    
+}else{
+    console.log(chalk.red("you are wrong guessed!"));
+    
+}
