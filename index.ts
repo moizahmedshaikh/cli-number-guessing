6512587a77d61370cc73a03c2f312c9b917811be
Moiz Ahmed <moizaman905@gmail.com>
2024-04-30 15:40:27 -0700

    updated

--- index.ts
+++ index.ts
@@ -3,24 +3,37 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
-console.log("welcome to Guessing Number Game");
+console.log(
+  chalk.italic.bgGray("<<<------welcome to Guessing Number Game------>>>")
+);
+console.log("-----------------------------------------------");
 
-let gameNumber = Math.floor(Math.random() * 6 + 1);
+let condition = true;
+while (condition) {
+  let gameNumber = Math.floor(Math.random() * 6 + 1);
 
-const answer = await inquirer.prompt([
+  const answer = await inquirer.prompt([
     {
-       name : "userGuessedNumber",
-       type : "number",
-       message : "Please guess a number between 1 To 6 :",
-    }
-])
-if(gameNumber === answer.userGuessedNumber){
-    console.log(chalk.yellowBright("Congratulatios! you guessed right number"));
-    
-    
-    
-}else{
+      name: "userGuessedNumber",
+      type: "number",
+      message: "Please guess a number between 1 To 6 :",
+    },
+  ]);
+  if (gameNumber === answer.userGuessedNumber) {
+    console.log(
+      chalk.italic.yellowBright(
+        "Congratulatios! you Winner: You guessed right number"
+      )
+    );
+    console.log(
+      chalk.italic.bgRed("<<<--------------Game End-------------->>>")
+    );
+
+    condition = false;
+  } else {
     console.log(chalk.red("you are wrong guessed!"));
     console.log(chalk.blue(`computer Guessed Number is : ${gameNumber}`));
-    
+    console.log("-----------------------------------------------");
+  }
 }
+
